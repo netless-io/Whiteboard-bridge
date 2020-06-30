e1435d1dbb3be8db2ebd980ef0b0bc37914020dd
leavesster <11785335+leavesster@users.noreply.github.com>
2020-06-30 14:40:23 +0800

    update web-sdk

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
 
-import "white-web-sdk/style/index.css";
-
 ReactDOM.render(
   <App />,
   document.getElementById("root") as HTMLElement,
