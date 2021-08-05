46e107a45e067f56e75f8945ff717b1428ecd6d2
leavesster <11785335+leavesster@users.noreply.github.com>
2021-08-05 13:39:13 +0800

    support modules

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,7 @@ module.exports = {
     ],
     "plugins": [
         "@babel/proposal-class-properties",
-        "@babel/proposal-object-rest-spread"
+        "@babel/proposal-object-rest-spread",
+        "@babel/plugin-transform-modules-commonjs"
     ]
 }
