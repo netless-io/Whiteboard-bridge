6b351c34f22c2a61b03d2ba4eec4d9edb9f3221e
leavesster <11785335+leavesster@users.noreply.github.com>
2022-01-25 18:28:13 +0800

    webpack5

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,12 @@
  */
 
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 config = {
   entry: './src/index',
@@ -15,21 +16,40 @@ config = {
     path: path.join(__dirname, '/build'),
     filename: `[name].[contenthash:8].js`
   },
+  target: ["web", "es5"],
   resolve: {
     extensions: ['.ts', '.tsx', '.js', "cjs"],
-    mainFields: ["main", "module"]
+    mainFields: ["main", "module"],
+    fallback: {
+      buffer: "buffer",
+    }
   },
   optimization: {
-    minimizer: [new TerserJSPlugin({extractComments: false}), new OptimizeCSSAssetsPlugin({})],
-    moduleIds: 'hashed',
+    minimizer: [new TerserJSPlugin({extractComments: false, parallel: true}), new CssMinimizerPlugin({})],
+    moduleIds: 'deterministic',
     runtimeChunk: 'single',
     splitChunks: {
+      chunks: "all",
       cacheGroups: {
-        white : {
+        white: {
           test: /[\\/]node_modules[\\/](white-web-sdk)[\\/]/,
           name: 'web-sdk',
           chunks: 'all',
           priority: 10,
+          reuseExistingChunk: true
+        },
+        video: {
+          test: /video/,
+          name: 'video',
+          chunks: 'all',
+          priority: 7,
+          reuseExistingChunk: true
+        },
+        netless: {
+          test: /@netless/,
+          name: 'netless',
+          chunks: 'all',
+          priority: 7,
           reuseExistingChunk: true
         },
         vendors: {
@@ -43,33 +63,23 @@ config = {
     }
   },
   plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
-    new MiniCssExtractPlugin({
-      filename: `[name].css`,
-      chunkFilename: `[name].[contenthash:8].css`,
-      ignoreOrder: false, // Enable to remove warnings about conflicting order
-    }),
+    new MiniCssExtractPlugin()
   ],
   module: {
     rules: [
       {
         test: /\.(ts|js|cjs)x?$/,
-        use: {
-          loader: 'babel-loader'
-        },
-      },
-      {
-        test: /\.less$/,
-        use: [{
-          loader: MiniCssExtractPlugin.loader
-        }, {
-          loader: 'css-loader' // translates CSS into CommonJS
-        }, {
-          loader: 'less-loader' // compiles Less to CSS
-        }]
+        use: [
+          "thread-loader",
+          'babel-loader',
+        ],
       },
       {
         test: /\.css$/,
@@ -79,7 +89,15 @@ config = {
         test: /\.(svg|png)/,
         use: ['file-loader']
       }
-    ]
+    ],
+    unknownContextCritical: false,
+  },
+  cache: {
+    type: "filesystem",
+    // 手动修改 node_modules 缓存不会失效。可以通过手动修改 config 或者删除 .cache 文件来触发，同时观察文件名是否有变化。
+    buildDependencies: {
+      config: [__filename],
+    },
   }
 };
 
