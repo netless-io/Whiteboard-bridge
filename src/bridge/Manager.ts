cab56a8088ffb7d094006e0faa64d87cce86d623
leavesster <11785335+leavesster@users.noreply.github.com>
2022-03-01 12:15:29 +0800

    refactor manager callback

--- src/bridge/Manager.ts
+++ src/bridge/Manager.ts
@@ -1,46 +1,35 @@
 import { WindowManager } from "@netless/window-manager";
 import "../utils/ParamTypes";
 import dsBridge from "dsbridge";
-import { Camera, RoomState, Size } from "white-web-sdk";
+import { DisplayerState } from "white-web-sdk";
+import { TeleBoxState } from "@netless/telebox-insider";
+
+type NativeDisplayerState = DisplayerState & {
+    windowBoxState: TeleBoxState;
+}
+
+function fireDisplayerState(state: Partial<NativeDisplayerState>) {
+    if (window.room) {
+        dsBridge.call("room.fireRoomStateChanged", JSON.stringify(state));
+    } else if (window.player) {
+        dsBridge.call("player.onPlayerStateChanged", JSON.stringify(state));
+    }
+}
 
 export function registerManager(manager: WindowManager, logger: (funName: string, ...param: any[]) => void): void {
     window.manager = manager;
-    // 多窗口，cameraState 不可用，通过这种方式，来替代
-    manager.mainView.callbacks.on("onCameraUpdated", (camera: Camera) => {
-        const size = manager.mainView.size;
-        const modifyState: Partial<RoomState> = {cameraState: {...size, ...camera}};
-        if (window.room) {
-            dsBridge.call("room.fireRoomStateChanged", JSON.stringify(modifyState));
-        } else if (window.player) {
-            dsBridge.call("player.onPlayerStateChanged", JSON.stringify(modifyState));
-        }
-    });
 
-    manager.mainView.callbacks.on("onSizeUpdated", (size: Size) => {
-        const camera = manager.mainView.camera;
-        const modifyState: Partial<RoomState> = {cameraState: {...size, ...camera}};
-        if (window.room) {
-            dsBridge.call("room.fireRoomStateChanged", JSON.stringify(modifyState));
-        } else if (window.player) {
-            dsBridge.call("player.onPlayerStateChanged", JSON.stringify(modifyState));
-        }
+    // 多窗口模式下，原有的 cameraState 意义丢失，通过这种方式，来替代显示 mainView 的 cameraState
+    manager.emitter.on("cameraStateChange", cameraState => {
+        fireDisplayerState({cameraState});
     });
 
     manager.emitter.on("sceneStateChange", sceneState => {
-        if (window.room) {
-            dsBridge.call("room.fireRoomStateChanged", JSON.stringify({sceneState}));
-        } else if (window.player) {
-            dsBridge.call("player.onPlayerStateChanged", JSON.stringify({sceneState}));
-        }
+        fireDisplayerState({sceneState});
     });
 
     manager.emitter.on("boxStateChange", state => {
-        const modifyState = { windowBoxState: state }
-        if (window.room) {
-            dsBridge.call("room.fireRoomStateChanged", JSON.stringify(modifyState));
-        } else if (window.player) {
-            dsBridge.call("player.onPlayerStateChanged", JSON.stringify(modifyState));
-        }
+        fireDisplayerState({ windowBoxState: state });
     });
     
     manager.emitter.on("canRedoStepsChange",canRedoSteps => {
@@ -49,5 +38,4 @@ export function registerManager(manager: WindowManager, logger: (funName: string
     manager.emitter.on("canUndoStepsChange",canUndoSteps => {
         dsBridge.call("room.fireCanUndoStepsUpdate", canUndoSteps);
     });
-    window.manager = manager;
 }
