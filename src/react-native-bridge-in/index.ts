1ca40471f5553228be7b6b94ebfb6f938cd7eddd
buhe <bugu1986@126.com>
2022-04-06 13:05:28 +0800

    u

--- src/react-native-bridge-in/index.ts
+++ src/react-native-bridge-in/index.ts
@@ -1,8 +1,26 @@
 export default class Bridge {
-    public call(method, args, cb) {
-
+    // methods: Map<string, any> = new Map()
+    public call(method: string, args) {
+        // call out
+        (window as any).ReactNativeWebView.postMessage("s-m"); 
     }
-    public register(name, fun, asyn) {
+    public register(name: string, fun: any) {
+        window[name] = fun;
+    }
 
+    public init(){
+        window.onmessage = function (event) {
+            console.log(event);
+            let potocol: string = event.data;
+            let dser: string[] = potocol.split("|");
+            let method = dser[3];
+            let args = dser[4];
+
+            let fun = window[method];
+            fun.apply(args);
+
+
+        }
+        
     }
 }
