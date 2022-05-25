26db37737a90a8b97cd87affb4f9ef3ef9467fb6
buhe <bugu1986@126.com>
2022-05-25 16:20:30 +0800

    read config

--- gen/index.ts
+++ gen/index.ts
@@ -1,9 +1,20 @@
+import fs from 'fs'
+let config = {
+    "rpc": {
+        "SDKBridge": {
+            "SDKBridge": ["newWhiteSdk"]
+        }
+    },
+    "event": {}
+};
 function enter() {
     // load config and parse
-
+    console.log(config);
     // parse ast
 
     // gen ir
 
     // gen ts
 }
+
+enter();
