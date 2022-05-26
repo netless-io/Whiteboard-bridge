704601c5dbf769cb3f134cc1445e468dfad3a416
buhe <bugu1986@126.com>
2022-05-26 15:28:38 +0800

    parse ts file.

--- gen/index.ts
+++ gen/index.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import path from "path";
 import ts from "typescript";
 let config = {
     "rpc": {
@@ -13,13 +14,14 @@ function enter() {
     console.log(config);
     // parse ast
     for (let rpc_file in config.rpc) {
-        // const dTsFile = fs.readFileSync(resolve(__dirname, "", rpc_file), 'utf-8');
-        // const sourceFile = ts.createSourceFile(
-        //     'sdk.ts',                      
-        //     dTsFile,                        
-        //     ts.ScriptTarget.Latest          
-        // )
-        console.log("read ts file " + rpc_file + ".d.ts");
+        const filePath = path.resolve(process.cwd(), "dist/src/bridge/" + rpc_file + ".d.ts");
+        const dTsFile = fs.readFileSync(filePath, 'utf-8');
+        const sourceFile = ts.createSourceFile(
+            rpc_file + ".d.ts",                      
+            dTsFile,                        
+            ts.ScriptTarget.Latest          
+        )
+        console.log("read ts file " + JSON.stringify(sourceFile));
         let clazz = config.rpc[rpc_file]; // like "SDKBridge": ["newWhiteSdk"]
         for (let clazzName in clazz) {
             let methods = clazz[clazzName];
