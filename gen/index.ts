ab9573f456ba77b77a4bb4fe09057a3fe44c1222
buhe <bugu1986@126.com>
2022-05-28 15:23:09 +0800

    remove comments

--- gen/index.ts
+++ gen/index.ts
@@ -23,14 +23,6 @@ function enter() {
     // parse ast
     for (let rpc_file in config.rpc) {
         const filePath = path.resolve(process.cwd(), "dist/src/bridge/" + rpc_file + ".d.ts");
-        // const dTsFile = fs.readFileSync(filePath, 'utf-8');
-        // console.log(dTsFile);
-        // const sourceFile = ts.createSourceFile(
-        //     rpc_file + ".d.ts",                      
-        //     dTsFile,                        
-        //     ts.ScriptTarget.Latest          
-        // )
-        // console.log("read ts file " + JSON.stringify(sourceFile));
         let clazz = config.rpc[rpc_file]; // like "SDKBridge": ["newWhiteSdk"]
         for (let clazzName in clazz) {
             let methods = clazz[clazzName];
