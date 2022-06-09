32d0eff3e1c1231e6a4ca438c1c953a91d7862b3
leavesster <11785335+leavesster@users.noreply.github.com>
2022-06-09 17:28:11 +0800

    refactor: debug use window function

--- src/Debug.ts
+++ src/Debug.ts
@@ -8,19 +8,19 @@ export function addDebugFunctions() {
     function testRoom() {
         enableReport(true);
         const sdkParams = {log: true, userCursor: true, __platform: "bridge", appIdentifier, useMultiViews: true};
-        (window as any)._dsaf._obs.sdk.newWhiteSdk(sdkParams, () => {});
+        (window as any).newWhiteSdk(sdkParams, () => {});
         const roomParams = {uuid: testRoomUUID, uid: "0", roomToken: testRoomToken, userPayload: {
             avatar: "https://white-pan.oss-cn-shanghai.aliyuncs.com/40/image/mask.jpg"
         }};
-        (window as any)._dsaf._obs.sdk.joinRoom(roomParams, ()=>{});
+        (window as any).joinRoom(roomParams, ()=>{});
     }
 
     function testReplay() {
         enableReport(true);
         const sdkParams = {log: true, userCursor: true, __platform: "bridge", appIdentifier, useMultiViews: true};
-        (window as any)._dsaf._obs.sdk.newWhiteSdk(sdkParams, () => {});
+        (window as any).newWhiteSdk(sdkParams, () => {});
         const replayParams = { room: testRoomUUID, roomToken: testRoomToken };
-        (window as any)._dsaf._obs.sdk.replayRoom(replayParams, ()=>{});
+        (window as any).replayRoom(replayParams, ()=>{});
     }
 
     window.testRoom = testRoom;
