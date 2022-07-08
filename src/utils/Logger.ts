0c7b2ab34c253e8382675d68d9de36d28731732d
xuyunshi <405029644@qq.com>
2022-07-08 18:00:27 +0800

    Add delayed log for `newWhiteSDK` and `joinRoom`

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -11,16 +11,29 @@ export function logger(funName: string, ...params: any[]) {
     }
 }
 
+let delayedLogs: string[][] = [];
+export function reportDelayedLog() {
+    delayedLogs.forEach(log => {
+        if (window.room) {
+            (window.room as any).logger.info(...log);
+        }
+    })
+    delayedLogs = [];
+}
+
 function report(funName: string, ...params: any[]) {
+    // sdk 的 logger，会直接使用 toString 方法，进行转换。Object 的 toString 直接是 "[object Object]"，无法记录内容
+    params = params.map(v => {
+        if (typeof v === "object") {
+            return JSON.stringify(v);
+        }
+        return v;
+    });
     if (window.room) {
-        // sdk 的 logger，会直接使用 toString 方法，进行转换。Object 的 toString 直接是 "[object Object]"，无法记录内容
-        params = params.map(v => {
-            if (typeof v === "object") {
-                return JSON.stringify(v);
-            }
-            return v;
-        });
         (window.room as any).logger.info(funName, ...params);
+    } else {
+        const logItem = [funName, ...params];
+        delayedLogs.push(logItem);
     }
     let message;
     if (params.length === 0) {
