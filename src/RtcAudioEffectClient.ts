2adaeeb3b842f2f4ba9c2f84717dee9724f14b23
xuyunshi <405029644@qq.com>
2023-11-29 16:53:48 +0800

    Add logger for effect client

--- src/RtcAudioEffectClient.ts
+++ src/RtcAudioEffectClient.ts
@@ -1,6 +1,7 @@
 import { asyncCall, register } from "./bridge";
 import { EventEmitter } from "events";
 import { RTCEffectClient } from "@netless/slide-rtc-plugin";
+import { logger } from "./utils/Logger";
 
 export class RtcAudioEffectClient extends EventEmitter implements RTCEffectClient {
     static kAgoraAudioEffectStatePlaying = 810;
@@ -12,6 +13,7 @@ export class RtcAudioEffectClient extends EventEmitter implements RTCEffectClien
         super();
         register("rtc", {
             audioEffectCallback: (soundId: number, state: number) => {
+                logger("audioEffectCallback", { soundId, state });
                 switch (state) {
                     case RtcAudioEffectClient.kAgoraAudioEffectStatePlaying:
                         this.emit("play", soundId);
@@ -28,23 +30,28 @@ export class RtcAudioEffectClient extends EventEmitter implements RTCEffectClien
                 }
             },
             setEffectFinished: (soundId: number) => {
+                logger("setEffectFinished", { soundId });
                 this.emit("effectFinished", soundId);
             },
             effectDurationCallback: (filePath: string, duration: number) => {
+                logger("effectDurationCallback", { filePath, duration });
                 this.emit("duration", filePath, duration);
             }
         });
     }
 
     public getEffectsVolume(): Promise<number> {
+        logger("getEffectsVolume");
         return asyncCall("rtc.getEffectsVolume") as Promise<number>;
     }
 
     public setEffectsVolume(volume: number): Promise<number> {
+        logger("setEffectsVolume", { volume });
         return asyncCall("rtc.setEffectsVolume", volume) as Promise<number>;
     }
 
     public setVolumeOfEffect(soundId: number, volume: number): Promise<number> {
+        logger("setVolumeOfEffect", { soundId, volume });
         return asyncCall("rtc.setVolumeOfEffect", {
             soundId,
             volume,
@@ -61,6 +68,7 @@ export class RtcAudioEffectClient extends EventEmitter implements RTCEffectClien
         publish: boolean,
         startPos: number
     ): Promise<number> {
+        logger("playEffect", { soundId, filePath, loopCount, pitch, pan, gain, publish, startPos });
         return asyncCall("rtc.playEffect", {
             soundId,
             filePath,
@@ -74,10 +82,12 @@ export class RtcAudioEffectClient extends EventEmitter implements RTCEffectClien
     }
 
     public stopEffect(soundId: number): Promise<number> {
+        logger("stopEffect", { soundId });
         return asyncCall("rtc.stopEffect", soundId) as Promise<number>;
     }
 
     public stopAllEffects(): Promise<number> {
+        logger("stopAllEffects");
         return asyncCall("rtc.stopAllEffects") as Promise<number>;
     }
 
@@ -86,6 +96,7 @@ export class RtcAudioEffectClient extends EventEmitter implements RTCEffectClien
         filePath: string,
         startPos: number
     ): Promise<number> {
+        logger("preloadEffect", { soundId, filePath, startPos });
         return asyncCall("rtc.preloadEffect", {
             soundId,
             filePath,
@@ -94,30 +105,37 @@ export class RtcAudioEffectClient extends EventEmitter implements RTCEffectClien
     }
 
     public unloadEffect(soundId: number): Promise<number> {
+        logger("unloadEffect", { soundId });
         return asyncCall("rtc.unloadEffect", soundId) as Promise<number>;
     }
 
     public pauseEffect(soundId: number): Promise<number> {
+        logger("pauseEffect", { soundId });
         return asyncCall("rtc.pauseEffect", soundId) as Promise<number>;
     }
 
     public pauseAllEffects(): Promise<number> {
+        logger("pauseAllEffects");
         return asyncCall("rtc.pauseAllEffects") as Promise<number>;
     }
 
     public resumeEffect(soundId: number): Promise<number> {
+        logger("resumeEffect", { soundId });
         return asyncCall("rtc.resumeEffect", soundId) as Promise<number>;
     }
 
     public resumeAllEffects(): Promise<number> {
+        logger("resumeAllEffects");
         return asyncCall("rtc.resumeAllEffects") as Promise<number>;
     }
 
     public getEffectDuration(url: string): Promise<number> {
+        logger("getEffectDuration", { url });
         return asyncCall("rtc.getEffectDuration", url) as Promise<number>;
     }
 
     public setEffectPosition(soundId: number, pos: number): Promise<number> {
+        logger("setEffectPosition", { soundId, pos });
         return asyncCall("rtc.setEffectPosition", {
             soundId,
             pos,
