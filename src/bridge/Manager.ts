601247c21c6a53ddbfb59ed7db9076659c090d47
aderan <fenglibinjie@gmail.com>
2025-06-17 11:44:38 +0800

    add appState with focusedId and appIds

--- src/bridge/Manager.ts
+++ src/bridge/Manager.ts
@@ -17,6 +17,14 @@ export function addManagerListener(manager: WindowManager, logger: (funName: str
     }
 }
 
+export function createAppState() {
+    const manager = window.manager;
+    return {
+        focusedId: manager?.focused,
+        appIds: manager?.apps ? Object.keys(manager.apps) : [],
+    };
+}
+
 function addRoomListener(manager: WindowManager, logger: (funName: string, ...param: any[]) => void, handler: RoomCallbackHandler) {
     // 多窗口模式下，原有的 cameraState 意义丢失，通过这种方式，来替代显示 mainView 的 cameraState
     manager.emitter.on("cameraStateChange", cameraState => {
@@ -38,12 +46,23 @@ function addRoomListener(manager: WindowManager, logger: (funName: string, ...pa
     manager.emitter.on("canRedoStepsChange",canRedoSteps => {
         handler.onCanRedoStepsUpdate(canRedoSteps);
     });
+    
     manager.emitter.on("canUndoStepsChange",canUndoSteps => {
         handler.onCanUndoStepsUpdate(canUndoSteps);
     });
 
     manager.emitter.on("loadApp", event => {
         logger("loadApp", event);
+    });
+
+    manager.emitter.on("focusedChange", focusedId => {
+        logger("focusedChange", focusedId);
+        handler.onRoomStateChanged({ appState: createAppState()});
+    })
+
+    manager.emitter.on("appsChange", apps => {
+        logger("appsChange", JSON.stringify(apps));
+        handler.onRoomStateChanged({ appState: createAppState()});
     });
 }
 
